01031b07a1b0caad229fb157659fe6632ef7aba8
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2022-02-26 22:48:32 +0100

    add fixtures for User entity

---
+++ symfony/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    /** @var UserPasswordHasherInterface */
+    protected $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $users = [
+            [
+                "email" => "admin@local.com",
+                "roles" => ["ROLE_ADMIN"],
+            ]
+        ];
+
+        foreach ($users as $key => $value) {
+            $user = new User();
+            $user->setEmail($value["email"]);
+            $user->setPassword($this->encoder->hashPassword($user, "admin"));
+
+            if (is_array($value['roles'])) {
+                foreach ((array)$value['roles'] as $_role) {
+                    $user->addRole($_role);
+                }
+            } else {
+                $user->addRole($value['role']);
+            }
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
