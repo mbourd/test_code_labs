930b83167f87f60a892eff55b330bcd602d912d9
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2022-02-27 20:48:32 +0100

    implements trycatch / display flash bag message on views / update translations

--- symfony/src/Controller/AgencyController.php
+++ symfony/src/Controller/AgencyController.php
@@ -10,43 +10,55 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AgencyController extends AbstractController
 {
-    public function index(AgencyService $agencyService): Response
+    public function index(AgencyService $agencyService, TranslatorInterface $translator ): Response
     {
-        return $this->render('agency/index.html.twig', [
-            'agencies' => $agencyService->getAll(),
-        ]);
+        try {
+            return $this->render('agency/index.html.twig', [
+                'agencies' => $agencyService->getAll(),
+            ]);
+        } catch (\Throwable $e) {
+            $this->addFlash('danger', $translator->trans("messages.errors.agency.getall"));
+            $this->redirectToRoute("admin");
+        }
     }
 
-    public function new(Request $request, AgencyService $agencyService): Response
+    public function new(Request $request, AgencyService $agencyService, TranslatorInterface $translator): Response
     {
-        $agency = new Agency();
-        $form = $this->createForm(AgencyType::class, $agency);
-        $form->handleRequest($request);
-        $errors = [];
+        try {
+            $agency = new Agency();
+            $form = $this->createForm(AgencyType::class, $agency);
+            $form->handleRequest($request);
+            $errors = [];
 
-        if ($form->isSubmitted()) {
-            $errors = $agencyService->getFormErrors($form);
+            if ($form->isSubmitted()) {
+                $errors = $agencyService->getFormErrors($form);
 
-            if (count($errors) === 0) {
-                $agencyService->createAgency([
-                    "name" => $agency->getName(),
-                    "address" => $agency->getAddress(),
-                    "latitude" => $agency->getLatitude(),
-                    "longitude" => $agency->getLongitude(),
-                ]);
+                if (count($errors) === 0) {
+                    $agencyService->createAgency([
+                        "name" => $agency->getName(),
+                        "address" => $agency->getAddress(),
+                        "latitude" => $agency->getLatitude(),
+                        "longitude" => $agency->getLongitude(),
+                    ]);
+                    $this->addFlash("success", $translator->trans("messages.success.agency.addNew"));
 
-                return $this->redirectToRoute('app_agency_index', [], Response::HTTP_SEE_OTHER);
+                    return $this->redirectToRoute('app_agency_index', [], Response::HTTP_SEE_OTHER);
+                }
             }
-        }
 
-        return $this->renderForm('agency/new.html.twig', [
-            'agency' => $agency,
-            'form' => $form,
-            'errors' => $errors
-        ]);
+            return $this->renderForm('agency/new.html.twig', [
+                'agency' => $agency,
+                'form' => $form,
+                'errors' => $errors
+            ]);
+        } catch (\Throwable $e) {
+            $this->addFlash('danger', $translator->trans("messages.errors.agency.addNew"));
+            $this->redirectToRoute("admin");
+        }
     }
 
     public function show(Agency $agency): Response
@@ -56,35 +68,47 @@ class AgencyController extends AbstractController
         ]);
     }
 
-    public function edit(Request $request, Agency $agency, AgencyService $agencyService): Response
+    public function edit(Request $request, Agency $agency, AgencyService $agencyService, TranslatorInterface $translator): Response
     {
-        $form = $this->createForm(AgencyType::class, $agency);
-        $form->handleRequest($request);
-        $errors = [];
+        try {
+            $form = $this->createForm(AgencyType::class, $agency);
+            $form->handleRequest($request);
+            $errors = [];
 
-        if ($form->isSubmitted()) {
-            $errors = $agencyService->getFormErrors($form);
+            if ($form->isSubmitted()) {
+                $errors = $agencyService->getFormErrors($form);
 
-            if (count($errors) === 0) {
-                $agencyService->updateAgency($agency);
+                if (count($errors) === 0) {
+                    $agencyService->updateAgency($agency);
+                    $this->addFlash("success", $translator->trans("messages.success.agency.edit"));
 
-                return $this->redirectToRoute('app_agency_index', [], Response::HTTP_SEE_OTHER);
+                    return $this->redirectToRoute('app_agency_index', [], Response::HTTP_SEE_OTHER);
+                }
             }
-        }
 
-        return $this->renderForm('agency/edit.html.twig', [
-            'agency' => $agency,
-            'form' => $form,
-            'errors' => $errors
-        ]);
+            return $this->renderForm('agency/edit.html.twig', [
+                'agency' => $agency,
+                'form' => $form,
+                'errors' => $errors
+            ]);
+        } catch (\Throwable $e) {
+            $this->addFlash('danger', $translator->trans("messages.errors.agency.edit"));
+            $this->redirectToRoute("admin");
+        }
     }
 
-    public function delete(Request $request, Agency $agency, AgencyService $agencyService): Response
+    public function delete(Request $request, Agency $agency, AgencyService $agencyService, TranslatorInterface $translator): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $agency->getId(), $request->request->get('_token'))) {
-            $agencyService->deleteAgency($agency);
-        }
+        try {
+            if ($this->isCsrfTokenValid('delete' . $agency->getId(), $request->request->get('_token'))) {
+                $agencyService->deleteAgency($agency);
+                $this->addFlash("success", $translator->trans("messages.success.agency.delete"));
+            }
 
-        return $this->redirectToRoute('app_agency_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_agency_index', [], Response::HTTP_SEE_OTHER);
+        } catch (\Throwable $e) {
+            $this->addFlash('danger', $translator->trans("messages.errors.agency.delete"));
+            $this->redirectToRoute("admin");
+        }
     }
 }
